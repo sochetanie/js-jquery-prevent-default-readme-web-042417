17bc32aff35b32b1bb90808fbfed92882d10b80a
sochetanie <allagorik2901@gmail.com>
2017-06-09 13:58:21 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   
   // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
-  });
+  // $('form').on('submit', function(){
+  //   var name = $('#name').val();
+  //   $("#hello").text("Hello, " + name);
+  // });
 
   //examine event object
   // $('form').on('submit', function(event){
@@ -14,11 +14,11 @@ $(document).ready(function(){
   //   event.preventDefault();
   // });
 
-  // stop page refresh
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   event.preventDefault();
-  // });
+  stop page refresh
+  $('form').on('submit', function(event){
+    var name = $('#name').val();
+    $("#hello").text("Hello, " + name);
+    event.preventDefault();
+  });
 
 });
